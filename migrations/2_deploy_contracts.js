b88393f8abc7d625aa4f7794d6320d16c6c15b99
guillermoFragachan <3945636+guillermoFragachan@users.noreply.github.com>
2022-01-23 03:48:30 +0100

    c

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,12 @@
+const Lottery = artifacts.require("./Lottery.sol");
+
+module.exports = async function(deployer, network, accounts) {
+
+
+ 
+  await deployer.deploy(Lottery)
+  const lottery = await Lottery.deployed()
+
+ console.log(lottery)
+
+};
